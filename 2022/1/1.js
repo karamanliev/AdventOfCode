3fb659952531a0dc8b64262580b5fa111ad62c3b
Hristo Karamanliev <karamanliev@gmail.com>
2023-07-09 11:08:45 +0300

    greatly simplify day1 solution

--- 2022/1/1.js
+++ 2022/1/1.js
@@ -6,66 +6,31 @@ function getInput() {
             if (err) {
                 reject(err)
             } else {
-                resolve(data)
+                resolve(data.toString())
             }
         })
     })
 }
 
-function getChunksSize(data) {
-    const chunksSize = data.reduce((acc, curr, index) => {
-        if (curr === '') {
-            acc.push(index)
-        }
+getInput().then((input) => {
+    const stringifiedCaloryArray = input
+        .split('\n\n')
+        .map((line) => line.split('\n'))
 
-        return acc
-    }, [])
+    const reducedCaloryArr = stringifiedCaloryArray.map((subArr) =>
+        subArr.map((val) => Number(val)).reduce((acc, curr) => acc + curr)
+    )
 
-    return chunksSize
-}
+    const topThreeCaloriesArray = reducedCaloryArr
+        .sort((a, b) => b - a)
+        .slice(0, 3)
 
-function getChunks(data) {
-    const chunksSize = getChunksSize(data)
-
-    const chunks = chunksSize.map((chunkSize, index) => {
-        const lastChunkSize = chunksSize[index - 1] + 1
-        const start = index === 0 ? 0 : lastChunkSize
-
-        return data.slice(start, chunkSize)
-    })
-
-    return chunks
-}
-
-function getCalculatedCalories(data) {
-    const chunks = getChunks(data)
-
-    const caloriesArray = chunks.map((chunk) => {
-        return chunk
-            .map((calory) => parseInt(calory))
-            .reduce((acc, curr) => {
-                return acc + curr
-            })
-    })
-
-    return {
-        topCalory: Math.max(...caloriesArray),
-        topThreeCalories: caloriesArray
-            .sort((a, b) => b - a)
-            .slice(0, 3)
-            .reduce((acc, curr) => {
-                return acc + curr
-            }),
-    }
-}
-
-getInput().then((res) => {
-    const resStringArray = res.toString().split('\n')
-
-    const topCalory = getCalculatedCalories(resStringArray).topCalory
-    const topThreeCalories = getCalculatedCalories(resStringArray).topThreeCalories
+    const topCalory = topThreeCaloriesArray[0]
+    const topThreeCaloriesSum = topThreeCaloriesArray.reduce(
+        (acc, curr) => acc + curr
+    )
 
     console.log(
-        `Top Calory: ${topCalory}\nTop Three Calory: ${topThreeCalories}`
+        `Top Calory: ${topCalory}\nTop Three Calories: ${topThreeCaloriesSum}`
     )
 })
