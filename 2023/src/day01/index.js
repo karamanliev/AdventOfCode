031ac8c70697a36a39b5bc60ad46f681540071a6
Hristo Karamanliev <karamanliev@gmail.com>
2023-12-03 00:10:54 +0200

    add day 1, part 1 solution

--- 2023/src/day01/index.js
+++ 2023/src/day01/index.js
@@ -4,12 +4,80 @@ const parseInput = (rawInput) => rawInput
 
 const part1 = (rawInput) => {
   const input = parseInput(rawInput)
+  const inputLines = input.split("\n")
 
-  return
+  const onlyNumbersArr = inputLines.map((line) =>
+    line
+      .split("")
+      .filter((char) => !isNaN(char))
+      .join(""),
+  )
+
+  const firstAndLastNumberArr = onlyNumbersArr.map((number) => {
+    if (number.length === 1) {
+      return number + number
+    }
+
+    const firstAndLast = number[0] + number[number.length - 1]
+
+    return firstAndLast
+  })
+
+  const sum = firstAndLastNumberArr.reduce(
+    (acc, curr) => acc + parseInt(curr),
+    0,
+  )
+
+  return sum
 }
 
 const part2 = (rawInput) => {
   const input = parseInput(rawInput)
+  const inputLines = input.split("\n")
+  const namedNumbers = [
+    "one",
+    "two",
+    "three",
+    "four",
+    "five",
+    "six",
+    "seven",
+    "eight",
+    "nine",
+  ]
+
+  const onlyNumbersArr = inputLines.map((line) => {
+    // convert named numbers to values
+    // namedNumbers.forEach((number, index) => {
+    //   const value = (index + 1).toString()
+    //   line = line.replaceAll(number, value)
+
+    //   console.log(line)
+    // })
+    console.log(line.split("eight").join("8"))
+
+    const onlyNumbers = line
+      .split("")
+      .filter((char) => !isNaN(char))
+      .join("")
+
+    return onlyNumbers
+  })
+
+  const firstAndLastNumberArr = onlyNumbersArr.map((number) => {
+    if (number.length === 1) {
+      return number + number
+    }
+
+    const firstAndLast = number[0] + number[number.length - 1]
+
+    return firstAndLast
+  })
+
+  const sum = firstAndLastNumberArr.reduce(
+    (acc, curr) => acc + parseInt(curr),
+    0,
+  )
 
   return
 }
