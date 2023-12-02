423c9743dcba80e15968702335c3aa37a7d5e4e6
Hristo Karamanliev <karamanliev@gmail.com>
2023-12-02 22:42:28 +0200

    remove 2022

--- utils.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs')
-
-function getStringInput() {
-    const input = fs.readFileSync('./input.txt').toString().trim()
-
-    return input
-}
-
-module.exports = { getStringInput }
